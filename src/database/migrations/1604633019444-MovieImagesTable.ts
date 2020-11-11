471d61d669c2384aeba39f28c1b9876eae2d7381
Luis Martinez <mrluismartinez28@gmail.com>
2020-11-10 21:06:58 -0400

    fixing migrations

--- src/database/migrations/1604633019444-MovieImagesTable.ts
+++ src/database/migrations/1604633019444-MovieImagesTable.ts
@@ -42,48 +42,19 @@ export class MovieImagesTable1604633019444 implements MigrationInterface {
     );
 
 
-    await queryRunner.query(`CREATE TABLE "movie_likes"(
-      "id" int NOT NULL AUTO_INCREMENT,
-      "movie_id" int NOT NULL,
-      "user_id" int NOT NULL,
-      "created_at" datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-      "updated_at" datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-      PRIMARY KEY("id"),
-      UNIQUE KEY "IDX_713b139e9e925987dc8ae1d5ba"("movie_id", "user_id"),
-      KEY "FK_e1cbf8ff3b2c6b4102b5938d417"("user_id"),
-      CONSTRAINT "FK_3bc8eda4c33e53316619d61d7d4" FOREIGN KEY("movie_id") REFERENCES "movie"("id"),
-      CONSTRAINT "FK_e1cbf8ff3b2c6b4102b5938d417" FOREIGN KEY("user_id") REFERENCES "user"("id")
-    )`)
-    await queryRunner.query(`CREATE TABLE "movie_logs"(
-      "id" int NOT NULL AUTO_INCREMENT,
-      "title" varchar(255) NOT NULL,
-      "description" varchar(255) NOT NULL,
-      "sale_price" float NOT NULL,
-      "rental_price" float NOT NULL,
-      "movie_id" int NOT NULL,
-      "created_at" datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-      "updated_at" datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-      PRIMARY KEY("id"),
-      KEY "FK_c63bd816b1348ce6e6e3fd0f139"("movie_id"),
-      CONSTRAINT "FK_c63bd816b1348ce6e6e3fd0f139" FOREIGN KEY("movie_id") REFERENCES "movie"("id")
-    )`)
-
-
+    await queryRunner.query("CREATE TABLE `movie_likes`(`id` int NOT NULL AUTO_INCREMENT,`movie_id` int NOT NULL,`user_id` int NOT NULL,`created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),`updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),PRIMARY KEY(`id`),UNIQUE KEY `IDX_713b139e9e925987dc8ae1d5ba`(`movie_id`, `user_id`),KEY `FK_e1cbf8ff3b2c6b4102b5938d417`(`user_id`),CONSTRAINT `FK_3bc8eda4c33e53316619d61d7d4` FOREIGN KEY(`movie_id`) REFERENCES `movie`(`id`),CONSTRAINT `FK_e1cbf8ff3b2c6b4102b5938d417` FOREIGN KEY(`user_id`) REFERENCES `user`(`id`))")
+    await queryRunner.query("CREATE TABLE `movie_logs`(`id` int NOT NULL AUTO_INCREMENT,`title` varchar(255) NOT NULL,`description` varchar(255) NOT NULL,`sale_price` float NOT NULL,`rental_price` float NOT NULL,`movie_id` int NOT NULL,`created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),`updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),PRIMARY KEY(`id`),KEY `FK_c63bd816b1348ce6e6e3fd0f139`(`movie_id`),CONSTRAINT `FK_c63bd816b1348ce6e6e3fd0f139` FOREIGN KEY(`movie_id`) REFERENCES `movie`(`id`))")
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
 
+    await queryRunner.query("ALTER TABLE `movie_likes` DROP CONSTRAINT `FK_3bc8eda4c33e53316619d61d7d4`");
+    await queryRunner.query("ALTER TABLE `movie_likes` DROP CONSTRAINT `FK_e1cbf8ff3b2c6b4102b5938d417`");
     await queryRunner.query(
-      `ALTER TABLE "movie_likes" DROP CONSTRAINT "FK_3bc8eda4c33e53316619d61d7d4"`,
+      "ALTER TABLE `movie_logs` DROP CONSTRAINT `FK_c63bd816b1348ce6e6e3fd0f139`",
     );
-    await queryRunner.query(
-      `ALTER TABLE "movie_likes" DROP CONSTRAINT "FK_e1cbf8ff3b2c6b4102b5938d417"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "movie_logs" DROP CONSTRAINT "FK_c63bd816b1348ce6e6e3fd0f139"`,
-    );
-    queryRunner.query(`DROP TABLE movie_images`);
-    await queryRunner.query(`DROP TABLE "movie_likes"`);
-    await queryRunner.query(`DROP TABLE "movie_logs"`);
+    queryRunner.query("DROP TABLE `movie_images`");
+    await queryRunner.query("DROP TABLE `movie_like`");
+    await queryRunner.query("DROP TABLE `movie_logs`");
   }
 }
